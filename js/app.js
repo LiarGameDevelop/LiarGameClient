2ce84a7a78cf216ae7734cc9f0272b157e6f4e2c
JB <withbini2057@naver.com>
2022-11-22 23:54:47 +0900

    1.Fix ip 2.Fix http connection

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
-class ConnectionInfo{
-    constructor(senderId,roomId,personCount){
-        this.senderId=senderId
-        this.roomId=roomId
+class ConnectionInfo {
+    constructor(maxPersonCount, ownerId, roomId, roomName, personCount) {
+        this.maxPersonCount = maxPersonCount
+        this.ownerId = ownerId
+        this.roomId = roomId
+        this.roomName = roomName
         this.personCount = personCount
     }
 }
-class Message{
-    constructor(message, senderId, roomId){
-        this.message=message
-        this.senderId=senderId
-        this.roomId=roomId
+class Message {
+    constructor(message, senderId, roomId) {
+        this.message = message
+        this.senderId = senderId
+        this.roomId = roomId
     }
 }
 
 var stompClient = null
-var connectionInfo = null
-const socketEndPoint = 'http://127.0.0.1:8080/ws-connection'
+var _gconnectionInfo = null
+var senderId = guid()
+function getConnectionInfo() {
+    return _gconnectionInfo
+}
+const socketEndPoint = 'http://13.125.250.0:8080/ws-connection'
 
 function setConnected(connected) {
     $("#connect").prop("disabled", connected)
@@ -29,45 +35,120 @@ function setConnected(connected) {
     $("#greetings").html("")
 }
 
-function makeRoom(request) {
-    fetch('/localhost:8080/room',
-    {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: request
+//make room
+function requestMakeRoom(param) {
+    $.post({
+        url: 'http://13.125.250.0:8080/room',
+        //data: param,
+        data: JSON.stringify({ "maxPersonCount": 5, "roomName": "my room", "senderId": senderId }),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.info("requestMakeRoom : " + JSON.stringify(response))
+
+            _gconnectionInfo = new ConnectionInfo(response.maxPersonCount, response.ownerId, response.roomId, response.roomName, response.personCount)
+            $("#roomId").text(response.roomId)
+            connect(_gconnectionInfo)
+        },
+        error: function (response) {
+            console.info(JSON.stringify(response))
+        }
     })
 }
 
-function connect() {
-    var socket = new SockJS(socketEndPoint)
-    console.log("socket :" +JSON.stringify(socket))
-    stompClient = Stomp.over(socket)
-    console.log("stompClient :" +JSON.stringify(stompClient))
-    
-    stompClient.connect({}, function (frame) { //TODO: 방만들기
-        setConnected(true)
-        console.log('Connected: ' + frame)
-        connectionInfo = new ConnectionInfo(guid(),$("#roomId").val(),1)
-        console.info("connected room : "+$("#roomId").val())
-        stompClient.subscribe(`/subscribe/room/${connectionInfo.roomId}`, function (greeting) { //room 뒤에 uuid를 가지고 uuid에 전달된 메세지만 받을 수 있다.
-            //addChat(JSON.parse(greeting.body).content)
-            addChat(greeting)
-        })
+function enterRoom(param) {
+    $.post({
+        url: 'http://13.125.250.0:8080/room/enter',
+        //data: param,
+        data: { "roomId": param.roomId, "senderId": param.senderId },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.info(JSON.stringify(response))
+            connectionInfo = new ConnectionInfo(response)
+        },
+        error: function (response) {
+            console.info(JSON.stringify(response))
+        }
     })
 }
 
-function makeRoom() {
+function leaveRoom(param) {
+    $.post({
+        url: 'http://13.125.250.0:8080/room/enter',
+        //data: param,
+        data: { "roomId": param.roomId, "senderId": param.senderId },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.info(JSON.stringify(response))
+            connectionInfo = new ConnectionInfo(response)
+        },
+        error: function (response) {
+            console.info(JSON.stringify(response))
+        }
+    })
+}
+
+function deleteRoom() {
+    $.ajax({
+        type: "DELETE",
+        url: 'http://13.125.250.0:8080/room',
+        //TODO: change delete room param
+        data: { "roomId": connectionInfo.roomId, "ownerId": connectionInfo.senderId },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            disconnect()
+            console.info(JSON.stringify(response))
+        },
+        error: function (response) {
+            console.info(JSON.stringify(response))
+        }
+    })
+}
+
+//get room info
+function getRooms(roomId) {
+    $.get({
+        url: `http://13.125.250.0:8080/room?roomId=${roomId}`,
+        //data: param,
+        success: function (response) {
+            //connect()
+            console.info(JSON.stringify(response))
+        },
+        error: function (response) {
+            console.info(JSON.stringify(response))
+        }
+    })
+}
+
+function connect(connectionInfo) {
+    if (connectionInfo == null) {
+        if ($("#roomId").val() == null) {
+            console.error("Insert room id")
+            alert("Insert room id")
+            return
+        }
+        connectionInfo = new ConnectionInfo(8, guid(), $("#roomId").val(), "example room")
+        _gconnectionInfo = connectionInfo
+    }
+    console.info("connecting room : " + JSON.stringify(connectionInfo))
+
     var socket = new SockJS(socketEndPoint)
-    console.log("socket :" +JSON.stringify(socket))
+    console.log("socket :" + JSON.stringify(socket))
     stompClient = Stomp.over(socket)
-    console.log("stompClient :" +JSON.stringify(stompClient))
-    stompClient.connect({}, function (frame) { //TODO: 방만들기
+    console.log("stompClient :" + JSON.stringify(stompClient))
+
+    //동작안함.
+    stompClient.connect({}, function (frame) {
         setConnected(true)
         console.log('Connected: ' + frame)
-        connectionInfo = new ConnectionInfo(guid(),guid(),1)
-        console.info(`connected room id ${connectionInfo.ro}`)
-        stompClient.subscribe(`/subscribe/room/${connectionInfo.roomId}`, function (greeting) { //room 뒤에 uuid를 가지고 uuid에 전달된 메세지만 받을 수 있다.
-            //addChat(JSON.parse(greeting.body).content)
+        console.info('_gconnectionInfo :' + JSON.stringify(getConnectionInfo()))
+
+        console.info('_gconnectionInfo room id: ' + getConnectionInfo().roomId)
+
+        stompClient.subscribe(`/subscribe/room/${getConnectionInfo().roomId}`, function (greeting) {
             addChat(greeting)
         })
     })
@@ -83,14 +164,14 @@ function disconnect() {
 
 function guid() {
     function s4() {
-      return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+        return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
     }
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-}  
+}
 
 function sendName() {
-    var message = new Message($("#name").val(),connectionInfo.senderId,connectionInfo.roomId)
-    console.info(`send info : ${message}` )
+    var message = new Message($("#text-data").val(), senderId, _gconnectionInfo.roomId)
+    console.info(`send info : ${JSON.stringify(message)}`)
 
     stompClient.send('/publish/messages', {}, JSON.stringify(message))
 }
@@ -103,8 +184,8 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault()
     })
-    $( "#make" ).click(function() { makeRoom() })
-    $( "#connect" ).click(function() { connect() })
-    $( "#disconnect" ).click(function() { disconnect() })
-    $( "#send" ).click(function() { sendName() })
+    $("#make").click(function () { requestMakeRoom(); })
+    $("#connect").click(function () { connect() })
+    $("#disconnect").click(function () { disconnect() })
+    $("#send").click(function () { sendName() })
 })
